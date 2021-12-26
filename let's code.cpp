8c7a086612912df96c7947a3bfa2bb1666cb2bd9
ksjhiu <94771016+ksjhiu@users.noreply.github.com>
2021-12-26 16:08:47 +0530

    Add files via upload

---
+++ let's code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int age;
+	cout<<"Enter Age of a user:"<<endl;
+	cin>>age;
+	if(age>=18)
+	{
+		
+		cout<<"You are eligible"<<endl;
+	}
+	else 
+	cout <<"You are not eligible";
+	return 0;
+}
